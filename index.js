e8ba4609a68a3f86b54a1ccd614ed82d9a33d524
sethxd <seth.dehaan@gmail.com>
2016-03-31 08:56:14 -0400

    trying to add subreddit listing

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict'
 
+const _ = require('lodash');
+const http = require('http');
+const req = require('request');
+
 module.exports = (pluginContext) => {
   const shell = pluginContext.shell
 
   function search (query, res) {
     const query_trim = query.trim()
+    console.log(query);
 
     if (query_trim.length === 0) {
       return
@@ -12,17 +17,39 @@ module.exports = (pluginContext) => {
 
     res.add({
       id: query_trim,
-      payload: 'open',
+      payload: 'search',
       title: query_trim,
-      desc: 'Search all of reddit'
+      desc: 'Search all of reddit for "' + query + '"'
     })
+
+    let subs = "http://api.reddit.com/subreddits/search?q=" + query_trim;
+    console.log(subs);
+    req.get(subs, function(data) {
+      console.log(data);
+      data = _.take(data, 5).map((x) => {
+       return {
+         id: x['data']['children']['data']['url'],
+         payload: 'subreddit',
+         title: x['data']['children']['data']['url'],
+         desc: "Go directly to the " + x['data']['children']['data']['title'] + " subreddit.",
+         icon: "#fa fa-bookmark"
+       };
+       res.add(data);
+     })
+   });
+
+
   }
 
   function execute (id, payload) {
-    if (payload !== 'open') {
+    if (payload == 'search') {
+      shell.openExternal(`https://www.reddit.com/search?q=${id}`);
+    } else if (payload == 'subreddit') {
+      shell.openExternal(`https://www.reddit.com${id}`);
+    } else {
       return
     }
-    shell.openExternal(`https://www.reddit.com/search?q=${id}`)
+
   }
 
   return { search, execute }
